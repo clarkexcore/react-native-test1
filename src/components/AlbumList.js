4a60e62e068f87a2c10d3ce8e1b4ecd65b143668
clarkexcore <clarkexcore@gmail.com>
2019-04-24 17:05:47 -0400

    Starting to use Axios

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Axios from 'axios';
 
 class AlbumList extends Component {
-    
+    state = {
+        albums: []
+    };
+
     componentWillMount() {
-        console.log('AHHHH MY DICK!');
+        Axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data}));
     }
     render() {
+        console.log(this.state);
         return (
             <View>
-                <Text>Hello</Text>
+                <Text>DESTROY</Text>
             </View>
         );
     }
