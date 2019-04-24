d79fcbc15bbe60ae864511032668ce6a960f9e48
clarkexcore <clarkexcore@gmail.com>
2019-04-24 16:22:11 -0400

    Some more components and what have you

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
+
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
 
 export default class App extends Component {
   state = {
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => (
-        <View>
-            <Text>Hello</Text>
-        </View>
-    );
+class AlbumList extends Component {
+    
+    componentWillMount() {
+        console.log('AHHHH MY DICK!');
+    }
+    render() {
+        return (
+            <View>
+                <Text>Hello</Text>
+            </View>
+        );
+    }
+}
 
 export default AlbumList;
